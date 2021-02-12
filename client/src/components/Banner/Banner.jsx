0cca26f320b1a65d76191cfff69c1e00c801b3d3
Pavel Khatyliou <hatylevpavel@gmail.com>
2021-02-12 17:02:53 +0300

    feat: get rid of scss

--- client/src/components/Banner/Banner.jsx
+++ client/src/components/Banner/Banner.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Banner = () => (
     <section className="row p-2 mb-3">
         <div className="col-md-12">
-            <div className="jumbotron text-white border border-white p-4 rounded">
+            <div className="jumbotron bg-dark text-white border border-white p-4 rounded">
                 <h2 className="display-4">Hello!</h2>
                 <p className="lead">This is the best cat shop in the world. All kinds of cat stuff can be found here.</p>
                 <hr className="my-4" />
--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import './Cards.scss'
 
 const Cards = () => (
     <section className="row">
         <div className="col-md-12 d-flex flex-wrap card-container justify-content-between">
-            <div className="card m-2 bg-dark text-white border rounded border-white">
+            <div style={{ width: "30%" }} className="card m-2 bg-dark text-white border rounded border-white">
                 <img class="card-img-top" src="https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1927&q=80" alt="Card cap" />
                 <div className="card-body">
                     <h5 className="card-title">Card title</h5>
@@ -12,7 +11,7 @@ const Cards = () => (
                     <a href="#" className="btn btn-primary">Open</a>
                 </div>
             </div>
-            <div className="card m-2 bg-dark text-white border rounded border-white">
+            <div style={{ width: "30%" }} className="card m-2 bg-dark text-white border rounded border-white">
                 <img class="card-img-top" src="https://images.unsplash.com/photo-1567170578400-9d182981f2a1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" alt="Card cap" />
                 <div className="card-body">
                     <h5 className="card-title">Card title</h5>
@@ -20,7 +19,7 @@ const Cards = () => (
                     <a href="#" className="btn btn-primary">Open</a>
                 </div>
             </div>
-            <div className="card m-2 bg-dark text-white border rounded border-white">
+            <div style={{ width: "30%" }} className="card m-2 bg-dark text-white border rounded border-white">
                 <img class="card-img-top" src="https://images.unsplash.com/photo-1535696588143-945e1379f1b0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt="Card cap" />
                 <div className="card-body">
                     <h5 className="card-title">Card title</h5>
@@ -29,7 +28,7 @@ const Cards = () => (
                 </div>
             </div>
         </div>
-    </section>
+    </section >
 )
 
 export default Cards
